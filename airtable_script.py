ab287b07161c745308af4bc5ecd50c870510faf6
Albin <josephAlbin12@gmail.com>
2019-07-20 16:27:20 -0500

    optimized script run time and updated algorithm to skip previously counted sign-ins

--- airtable_script.py
+++ airtable_script.py
@@ -1,44 +1,65 @@
 #requires: airtable-python wrapper
-
 from airtable import Airtable
 import pandas as pd
+import time
 
 base_key = 'app801qX3wwQdSaoT'
 api_key = 'keyVrbI2sZnDNOp5E'
 
-
-at = Airtable(base_key, 'E-Week Attendance 2019', api_key)
+at = Airtable(base_key, 'Imported table', api_key)
 points = Airtable(base_key, 'Leaderboard', api_key)
-
-orgs_list = ['AICHE', 'ASCE', 'ASME', 'BMESF', 'IEEE', 'SNaP', 'SWEEP',
-             'PGE', 'Longhorn Racing', 'AEI', 'EChO', 'ESW', 'HKN', 'KTE',
-             'SASE', 'SFE', 'TBP', 'TxTPEG', 'WIALD']
 
 def at_to_csv(df):
         df.to_csv('out.csv', index = False)
         
-def clear_score(df):
-        for index, row in df.iterrows():
-            if row['Organization'] in orgs_list:
-                record = points.match('Name', row['Organization'])
+def clear_score(pts):
+        for index, row in pts.iterrows():
+                record = points.match('Name', row['Name'])
                 fields = {'Sign-In Points': 0}
                 points.update(record['id'], fields)
         
-def update_score(df):        
-        clear_score(df)
-                
-        for index, row in df.iterrows():
-            eid = row['EID']
-            evnt = row['Event']
-            so = row['Signing-In or Signing-Out']
-            org = row['Organization']
-            for index, row in df.iterrows():
-                if eid == row['EID'] and evnt == row['Event'] and org == row['Organization'] and so != row['Signing-In or Signing-Out']:
-                    if row['Organization'] in orgs_list:
+def update_score(df, prev):
+        #cut out old sign ins
+        df2 = df.iloc[len(prev.index):]
+        
+        for index, row in df2.iterrows():
+                eid = row['UT EID']
+                evnt = row['Event']
+                so = row['Signing-In or Signing-Out']
+                org = row['Organization']
+                for index, row in df.iterrows():
+                    if eid == row['UT EID'] and evnt == row['Event'] and org == row['Organization'] and so != row['Signing-In or Signing-Out']:
                         record = points.match('Name', row['Organization'])
                         fields = {'Sign-In Points': record['fields']['Sign-In Points'] + .5}
                         points.update(record['id'], fields)
+                    
+#start timing                        
+start = time.time()
 
+#get records from pts and sign in
 records = at.get_all()
+pts_records = points.get_all()
+
+#create dataframes for pts, sign ins and previous sign ins
+try:
+    prev = pd.read_csv('out.csv')
+except:
+    prev = pd.DataFrame()
+    pass
+pts = pd.DataFrame.from_records((r['fields'] for r in pts_records))
 df = pd.DataFrame.from_records((r['fields'] for r in records))
-update_score(df)
+
+#clear and update scores
+clear_score(pts)
+update_score(df, prev)
+at_to_csv(df)
+
+#stop timing
+end = time.time()
+
+#calculate time
+hours, rem = divmod(end-start, 3600)
+minutes, seconds = divmod(rem, 60)
+print("{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds))
+
+
